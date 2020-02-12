51eeeb8f71360be38203033d7076b661123808e1
Jendnice <Pipeline38@hotmail.com>
2020-02-11 19:02:15 -0500

    create seed data, add in rack cors gem, uncomment cors.rb and update to allow all origins, run bundle install

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+
+# Seed data framework ready below. 
+# Just add in remaining info and you can run 'rake db:seed' to populate seed data:
+
+
+# Power.create([{
+#     name: "Earth",
+#     description: "",
+#     info: ""
+# },
+# {
+#     name: "Wind",
+#     description: "",
+#     info: ""
+# }])
+
+
+
+# Challenge.create([{
+#     name: "Befriend the Lorax",
+#     description: "",
+#     completed: false,
+#     power_id: 1
+# },
+# {
+#     name: "",
+#     description: "",
+#     completed: false,
+#     power_id: 2
+# },
+# {
+#     name: "",
+#     description: "",
+#     completed: false,
+#     power_id: 1
+# }])
