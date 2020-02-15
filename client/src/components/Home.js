fe544f974b9f0c317721724e352baf99d6dd7064
Jendnice <Pipeline38@hotmail.com>
2020-02-15 18:09:55 -0500

    add in animation feature for text and adjust styling in Home.js, NotFound.js and Home.css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,29 +1,35 @@
 import React, { Component } from 'react'
 import { Container} from 'semantic-ui-react'
 import './Home.css'
-// import { Button } from 'semantic-ui-react'
 
 class Home extends Component {
 
   render() {
     return (
       <div>
-        {/* <h1>Challenge Accepted!</h1> */}
         <div>
         <Container className="container">
-        <h3>The power is in your hands . . .</h3>
 
-        {/* <img src="https://cdn.shopify.com/s/files/1/1810/5253/articles/source.gif?v=1512068907"alt="animated rocket"></img> */}
-        <img src='https://i.pinimg.com/originals/e3/47/20/e3472086f5e52efcffa84c781684d53b.gif' alt="earth spinning"></img>
+          <div className="ele">
+            <h2>The power is in</h2>
+          </div>
 
-        <h3><i>what will you do with it?</i></h3>
+          <div className="ele1">
+            <h2>your hands . . .</h2>
+          </div>
+
+          <div className="ele2">
+            <h2>what will you do with it?</h2>
+          </div>
+
+        <img src='https://i.pinimg.com/originals/e3/47/20/e3472086f5e52efcffa84c781684d53b.gif' alt="earth spinning" className='worldGif'></img>
 
         </Container>
         <br/>
-        <button className="ui blue inverted animated button" onClick={() =>  window.location.href='/powers'}>
-        <div className="visible content">I'm ready</div>
-        <div className="hidden content"><i aria-hidden="true" className="arrow right icon"></i></div>
-        </button>
+          <button className="ui blue inverted animated button" onClick={() =>  window.location.href='/powers'}>
+            <div className="visible content">I'm ready</div>
+            <div className="hidden content"><i aria-hidden="true" className="arrow right icon"></i></div>
+          </button>
         </div>
       </div>
     )
--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -11,18 +11,23 @@ class NotFound extends Component {
         <div>
           <div>
           <Container className="container">
-  
-          <img src="https://media3.giphy.com/media/4Z7DE87md7MdOGvAAA/giphy.gif"alt="lost robot"></img>
-  
-          <h3>Oh, no! That page wasn't found.</h3>
-          <h3>But, don't worry! You can head home below.</h3>
-  
+
+          <div className="ele">
+            <h2>That page wasn't found.</h2>
+          </div>
+
+          <div className="ele2">
+            <h2>Head home below!</h2>
+          </div>
+
+          <img src="https://media3.giphy.com/media/4Z7DE87md7MdOGvAAA/giphy.gif" alt="lost robot" className="robotGif"></img>
+
           </Container>
           <br/>
-          <button className="ui blue inverted animated button" onClick={() =>  window.location.href='/'}>
-          <div className="visible content">Home</div>
-          <div className="hidden content"><i aria-hidden="true" className="arrow right icon"></i></div>
-          </button>
+            <button className="ui blue inverted animated button" onClick={() =>  window.location.href='/'}>
+              <div className="visible content">Home</div>
+              <div className="hidden content"><i aria-hidden="true" className="arrow right icon"></i></div>
+            </button>
           </div>
         </div>
       )
