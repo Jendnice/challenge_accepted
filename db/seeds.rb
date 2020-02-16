cdeecfe8a8bf882a5f994b515fa06c2ee3696c10
Jendnice <Pipeline38@hotmail.com>
2020-02-16 17:17:00 -0500

    update notes.md and add database info to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,38 +11,107 @@
 
 
 
-# Seed data framework ready below. 
-# Just add in remaining info and you can run 'rake db:seed' to populate seed data:
+# Seed data ready below! Run 'rake db:seed' to populate database.
 
 
 # Power.create([{
 #     name: "Earth",
+#     description: "Earth Needs Our Help!",
+#     info: "‘When human activities create greenhouse gases, Earth warms. This matters because oceans, land, air, plants, animals and energy from the Sun all have an effect on one another...In other words, Earth’s climate functions like one big, connected system.’*"
+# },
+# {
+#     name: "Heart",
+#     description: "A Home to Share",
+#     info: "Humans aren’t the only ones that call Earth home. It’s important to remember the our world has many inhabitants, and it’s our job to protect and support all of Earth’s creatures."
+# },
+# {
+#     name: "Wind,
+#     description: "A New Energy",
+#     info: "Wind Energy has many benefits and is a cost-competitive energy source. It’s a clean fuel source and is sustainable. Wind Energy actually a form of solar energy!"
+# },
+# {
+#     name: "Fire",
+#     description: "Stop the Spread",
+#     info: "Climate change negatively affects us in many ways. One of the most devastating effects is wildfires. ‘Climate change has been a key factor in increasing the risk and extent of wildfires in the Western United States.’*"
+# },
+# {
+#     name: "",
 #     description: "",
 #     info: ""
 # },
 # {
-#     name: "Wind",
-#     description: "",
-#     info: ""
+#     name: "Water",
+#     description: "Water, Water, Everywhere",
+#     info: "Our world has no shortage of ocean. In fact, the ocean covers 70% of Earth's surface. But that doesn’t mean we can overlook its importance!"
 # }])
 
 
 
 # Challenge.create([{
-#     name: "Befriend the Lorax",
-#     description: "",
+#     name: "Reduce, Reuse, Recycle",
+#     description: "Lower your impact on the world around you by minimizing your waste. Switch out your disposable papers and plastics for reusable materials!",
 #     completed: false,
 #     power_id: 1
 # },
 # {
-#     name: "",
-#     description: "",
+#     name: "Speak for the Trees",
+#     description: "Trees and forests do so much good for our world--and we’re destroying them! Lend a direct hand to the cause by planting a tree in your yard or at your school.",
+#     completed: false,
+#     power_id: 1
+# },
+# {
+#     name: "Adopt an (Animal) Friend",
+#     description: "‘Adopt’ an endangered animal of your choice through the World Wildlife Fund.*",
 #     completed: false,
 #     power_id: 2
 # },
 # {
+#     name: "Pay it Forward",
+#     description: "Do one thing today that helps someone. (Or helps a plant or animal!)",
+#     completed: false,
+#     power_id: 2
+# },
+# {
+#     name: "Knowledge is Power",
+#     description: "Want to learn more about wind and hear a true story of resilience and perseverance? Read The Boy Who Harnessed the Wind: Creating Currents of Electricity and Hope by William Kamkwamba and Bryan Mealer.",
+#     completed: false,
+#     power_id: 3
+# },
+# {
+#     name: "Knowledge is Power 2",
+#     description: "Reading not your thing? Don’t worry--William Kamkwamba’s story is now a film on Netflix. Check it out!",
+#     completed: false,
+#     power_id: 3
+# },
+# {
+#     name: "Remember Smokey the Bear...and his rules!",
+#     description: "‘Never play with matches or lighters,’ and ‘Make sure your campfire is completely out before leaving it,’ are two great rules to remember and practice!",
+#     completed: false,
+#     power_id: 4
+# },
+# {
+#     name: "Lend a Helping Hand",
+#     description: "Donate to the bushfire emergency in Australia! You can find more info on the WWF website.",
+#     completed: false,
+#     power_id: 4
+# },
+# {
 #     name: "",
 #     description: "",
 #     completed: false,
-#     power_id: 1
+#     power_id: 5
+# },
+# {
+#     name: "Save the Turtles!",
+#     description: "Just say no! To straws, that is. ‘Plastic is polluting oceans all over the planet...killing sea animals, and more.’* Ditching straws is a small way to make a big difference.",
+#     completed: false,
+#     power_id: 6
+# },
+# {
+#     name: "Beach Day",
+#     description: "Oceans and beaches are horribly polluted with plastics, trash, and other waste. Organize a beach clean-up day at a nearby beach and clean up any mess that doesn’t belong.",
+#     completed: false,
+#     power_id: 6
 # }])
+
+
